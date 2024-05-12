0c13700885b92ac332d627e77d45f00d3882aa34
jarvis1666 <ustimenko.s27@gmail.com>
2024-05-12 19:37:46 +0300

    ErrorPageBugFix

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom"
-import ErrorServer from "../../images/ErrorServer.jpg"
+import ErrorServer from "../../images/ErrorServer.webp"
 import css from "../Error/Error.module.css"
 
 export const ErrorForErrorPage = () => {
@@ -8,7 +8,7 @@ export const ErrorForErrorPage = () => {
             <img className={css.imgError} src={ErrorServer} alt="Зображення сервера" />
             <h2>The server is not responding!</h2>
             <p>Now we will clean the cobwebs, drive away the spiders - and everything will work.</p>
-            <NavLink to="/" className={css.btn}>Repeat</NavLink>
+            <NavLink to="/" className={css.btn}>Go to homepage</NavLink>
         </div>
     )
 }
