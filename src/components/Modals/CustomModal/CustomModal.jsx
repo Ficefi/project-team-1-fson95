a39d8c44e1da0dfaa39269eebe7739a5a4156997
MrRoik <138239220+MrRoik@users.noreply.github.com>
2024-05-14 16:13:28 +0300

    add modals
    add folder Modals
    create custom Modal
    create form add + edit water

---
+++ src/components/Modals/CustomModal/CustomModal.jsx
@@ -0,0 +1,44 @@
+import Modal from "react-modal";
+import { AiOutlineClose } from "react-icons/ai";
+import css from "./CustomModal.module.css";
+
+const customStyles = {
+  content: {
+    height: "auto",
+    width: "auto",
+    padding: "0",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    border: "none",
+    marginRight: "-50%",
+    backgroundSize: "cover",
+    backgroundColor: "transparent",
+    transform: "translate(-50%, -50%)",
+  },
+  overlay: {
+    background: "rgba(22, 22, 22, 0.8)",
+  },
+};
+
+const CustomModal = ({ isOpen, onClose, children }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      style={customStyles}
+      shouldCloseOnOverlayClick={true}
+      shouldCloseOnEsc={true}
+    >
+      <div className={css.modalBox}>
+        <span onClick={onClose} className={css.closedModal}>
+          <AiOutlineClose size="18" />
+        </span>
+        {children}
+      </div>
+    </Modal>
+  );
+};
+
+export default CustomModal;
