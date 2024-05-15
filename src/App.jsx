6fa367eca477ff69f024ff49368e27fbc9727b5d
MrRoik <138239220+MrRoik@users.noreply.github.com>
2024-05-15 14:16:49 +0300

    add folders
    add folders:
    -  /hook
    - redux /auth + /user
    - loader
    - PrivateRoute
    - RestrictedRoute
    update App & main.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,26 @@ import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import HomePage from './pages/HomePage/HomePage';
+import { LoaderPage } from './components/Loader/Loader';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { refreshUser } from './redux/auth/operations';
+import { useAuth } from './hooks';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const ErrorPage = lazy(() => import('./pages/ErrorPage'));
 
 const App = () => {
-  return (
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <LoaderPage />
+  ) : (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
@@ -12,6 +29,6 @@ const App = () => {
       </Route>
     </Routes>
   );
-}
+};
 
 export default App;
