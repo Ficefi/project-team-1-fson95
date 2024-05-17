c57902c906e542a6ac10f86050030e99d95b1589
Ficefi <murdzamarkian54@gmail.com>
2024-05-17 22:45:53 +0300

    change water operations

--- src/redux/water/operations.js
+++ src/redux/water/operations.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://aquatrack-api.onrender.com/water';
+axios.defaults.baseURL = 'https://aquatrack-api.onrender.com';
 
 export const addWater = createAsyncThunk(
   'water/addWater',
   async (values, thunkAPI) => {
     try {
-      const response = await axios.post('/', values);
+      const response = await axios.post('/water/', values);
       return response.data;
     } catch (er) {
       return thunkAPI.rejectWithValue(er.message);
@@ -19,7 +19,7 @@ export const deleteWater = createAsyncThunk(
   'water/deleteWater',
   async (waterId, thunkAPI) => {
     try {
-      const response = await axios.delete(`/${waterId}`);
+      const response = await axios.delete(`/water/${waterId}`);
       return response.data;
     } catch (er) {
       return thunkAPI.rejectWithValue(er.message);
@@ -31,7 +31,7 @@ export const updateWater = createAsyncThunk(
   'water/updateWater',
   async ({ _id, amountWater, time }, thunkAPI) => {
     try {
-      const response = await axios.put(`/${_id}`, {
+      const response = await axios.put(`/water/${_id}`, {
         amountWater,
         time,
       });
