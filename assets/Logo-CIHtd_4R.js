37c53f1e5505005198b20d94f52a8f5ab29c82d2
Ficefi <139962804+Ficefi@users.noreply.github.com>
2024-05-17 17:36:11 +0000

    Deploying to gh-pages from @ Ficefi/project-team-1-fson95@5544c7124bb4b828bdaa0b75320c6ba95529e6f5 🚀

---
+++ assets/Logo-CIHtd_4R.js
@@ -0,0 +1,2 @@
+import{j as o,L as n}from"./index-YNL5KR8k.js";const t="_logoContainer_fa5s9_1",a="_logo_fa5s9_1",s={logoContainer:t,logo:a},r=()=>o.jsx("div",{className:s.logoContainer,children:o.jsx(n,{to:"/",className:s.logo,children:"AQUATRACK"})});export{r as L};
+//# sourceMappingURL=Logo-CIHtd_4R.js.map
