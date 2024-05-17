0700cd7dee905cdb7bc61308f69282872e712d3c
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-05-17 14:40:09 +0200

    redux

---
+++ src/redux/auth/store.js
@@ -0,0 +1,50 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import { authReducer, verifyEmailSuccess } from './auth/slice';
+import { waterReducer } from './water/waterSlice';
+import { createBrowserHistory } from 'history';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+const history = createBrowserHistory();
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    water: waterReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+const persistor = persistStore(store, null, () => {
+  const urlParams = window.location.search;
+  const token = urlParams.substring(1);
+
+  if (token) {
+    store.dispatch(verifyEmailSuccess());
+    history.push('/finalTeamProject/tracker');
+  }
+});
+export { persistor };
+
