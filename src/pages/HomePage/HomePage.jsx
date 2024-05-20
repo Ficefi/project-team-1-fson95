9bed9d4a8e06f3ddefad288f00271733d7e0736d
Ivaskiv <viraivaskiv@gmail.com>
2024-05-20 21:51:04 +0300

    WaterDetailedInfo2 update waterList popover

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,9 @@
 import css from './HomePage.module.css';
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
 import AdvantagesSection from '../../components/AdvantagesSection/AdvantagesSection';
-import DailyInfo from '../../components/DailyInfo/DailyInfo';
 const HomePage = () => {
   return (
     <div className={css.container}>
-      <DailyInfo />
       <WelcomeSection />
       <AdvantagesSection />
     </div>
--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,11 +1,13 @@
 import WaterMainInfo from '../../components/WaterMainInfo/WaterMainInfo';
 import MonthInfo from '../../components/MonthInfo/MonthInfo';
 import css from './TrackerPage.module.css';
+import DailyInfo from '../../components/DailyInfo/DailyInfo';
 
 const TrackerPage = () => {
   return (
     <div className={css.tracker_container}>
       <WaterMainInfo />
+      <DailyInfo />
       <MonthInfo />
     </div>
   );
