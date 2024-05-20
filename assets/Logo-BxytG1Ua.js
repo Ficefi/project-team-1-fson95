b0f1ffef699a8ef65fa4df53cb1460551b3cf1f6
Ficefi <139962804+Ficefi@users.noreply.github.com>
2024-05-20 15:27:00 +0000

    Deploying to gh-pages from @ Ficefi/project-team-1-fson95@a5e020a8792e1ff70182f299caa231f651be9c72 🚀

---
+++ assets/Logo-BxytG1Ua.js
@@ -0,0 +1,2 @@
+import{j as o,L as n}from"./index-BS6VJh9R.js";const t="_logoContainer_fa5s9_1",a="_logo_fa5s9_1",s={logoContainer:t,logo:a},r=()=>o.jsx("div",{className:s.logoContainer,children:o.jsx(n,{to:"/",className:s.logo,children:"AQUATRACK"})});export{r as L};
+//# sourceMappingURL=Logo-BxytG1Ua.js.map
